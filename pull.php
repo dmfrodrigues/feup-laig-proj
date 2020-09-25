4fdf8606222c6620e3926031a1ef04487298e448
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-09-25 01:28:36 +0100

    Did my best with PHP

--- pull.php
+++ pull.php
@@ -2,8 +2,20 @@
 <html>
     <body>
         <?php
-        $output = shell_exec(git pull);
-        echo "$output";
+        $descriptorspec = array(
+            0 => array("pipe", "r"),
+            1 => array("pipe", "w"),
+            2 => array("pipe", "w")
+        );
+        $process = proc_open('bash -c "git pull"', $descriptorspec, $pipes, dirname(__FILE__), null);
+        $stdout = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
+        echo "stdout: \n";
+        var_dump($stdout);
+        echo "stderr: \n";
+        var_dump($stderr);
         ?>
     </body>
 </html>
