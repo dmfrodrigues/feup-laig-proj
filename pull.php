76a617cff0c8314015d2ec38f778ee944789a990
Diogo Rodrigues <up201806429@fe.up.pt>
2020-09-25 11:49:55 +0100

    Done with pull

--- pull.php
+++ pull.php
@@ -2,8 +2,20 @@
 <html>
     <body>
         <?php
-        $output = shell_exec('bash -c "git pull"');
-        echo "<pre>$output</pre>";
+	    $descriptorspec = array(
+            0 => array("pipe", "r"),
+            1 => array("pipe", "w"),
+            2 => array("pipe", "w")
+        );
+        $process = proc_open('ssh -tt -o "StrictHostKeyChecking=no" -i /usr/users2/2018/up201806429/public_html/ssh-private-keys/laig_t02_g04_pull -l up201806429 gnomo.fe.up.pt', $descriptorspec, $pipes, dirname(__FILE__), null);
+        $stdout = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
+        echo "stdout: \n";
+        echo "<pre>$stdout</pre>";
+        echo "stderr: \n";
+        echo "<pre>$stderr</pre>";
         ?>
     </body>
 </html>
---
+++ pull.sh
@@ -0,0 +1,2 @@
+cd ~/public_html/feup/3/1/laig-t02-g04
+git pull
