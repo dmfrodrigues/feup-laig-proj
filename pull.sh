172c89b540cb525a8c8b6aa2a545bfc101e114e4
Diogo Rodrigues <up201806429@fe.up.pt>
2020-09-25 12:56:03 +0100

    Fixed pull.sh

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 cd ~/public_html/feup/3/1/laig-t02-g04
 git pull
-echo "<!DOCTYPE html><html><body>Last updated $date</body></html>" > index.html
+echo "<\!DOCTYPE html><html><body>Last updated "`date`"</body></html>" > index.html
