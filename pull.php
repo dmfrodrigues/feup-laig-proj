1fdc013c111fc8cdb6c5dbef80d732744c0c301a
Diogo Rodrigues <up201806429@fe.up.pt>
2020-09-25 18:25:06 +0100

    Changed pull.php

--- pull.php
+++ pull.php
@@ -2,7 +2,12 @@
 <html>
     <body>
         <?php
-        $stdout = shell_exec('ssh -F ~/ssh-config-laig gnomo.fe.up.pt');
+        $cwd = getcwd();
+        $pos = strpos($cwd, "/public_html/");
+        $user_path = substr($cwd, 0, $pos);
+        $config_file_path = $user_path.'/ssh-config-laig';
+
+        $stdout = shell_exec('ssh -F '.$config_file_path.' gnomo.fe.up.pt');
         echo "<pre>$stdout</pre>";
         ?>
     </body>
