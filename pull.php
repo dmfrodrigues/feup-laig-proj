8625d46e7fa2eb9b571a7b417804acafeeb60e7e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-09-25 10:05:04 +0100

    Minor change to PHP

--- pull.php
+++ pull.php
@@ -2,20 +2,8 @@
 <html>
     <body>
         <?php
-        $descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("pipe", "w")
-        );
-        $process = proc_open('bash -c "git pull"', $descriptorspec, $pipes, dirname(__FILE__), null);
-        $stdout = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
-        $stderr = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
-        echo "stdout: \n";
-        var_dump($stdout);
-        echo "stderr: \n";
-        var_dump($stderr);
+        $output = shell_exec('bash -c "git pull"');
+        echo "<pre>$output</pre>";
         ?>
     </body>
 </html>
