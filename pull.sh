feafaa696b0c33a6cbb25ea89b958013e7a399e4
Diogo Rodrigues <up201806429@fe.up.pt>
2020-09-25 16:56:20 +0100

    Changed pull.sh

--- pull.sh
+++ pull.sh
@@ -1,4 +1,15 @@
-cd ~/public_html/feup/3/1/laig-t02-g04
+repo_name=laig-t02-g04
+deploy_dir=~/public_html/feup/3/1
+repo_path=$deploy_dir/$repo_name
+original_repo = ~/feup/3S1/LAIG/laig-t02-g04
+
+cd $original_repo
+
 git pull
 chmod 755 pull.sh
-echo "<!DOCTYPE html><html><body>Last updated "`date`"</body></html>" > index.html
+
+rm -rf $repo_path
+mkdir -p $repo_path
+cp lib TP1 TP2 TP3 pull.php $repo_path
+
+echo "<!DOCTYPE html><html><body>Last updated "`date`"</body></html>" > $repo_path/index.html
