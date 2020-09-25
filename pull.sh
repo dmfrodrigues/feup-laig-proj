3c2d58bb7fe2d42524c5b4b3be0867842ee6ab11
Diogo Rodrigues <up201806429@fe.up.pt>
2020-09-25 13:02:20 +0100

    Minor change

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 cd ~/public_html/feup/3/1/laig-t02-g04
 git pull
 chmod 755 pull.sh
-echo "<\!DOCTYPE html><html><body>Last updated "`date`"</body></html>" > index.html
+echo "<!DOCTYPE html><html><body>Last updated "`date`"</body></html>" > index.html
