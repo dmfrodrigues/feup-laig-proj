d18583f93b1a2c5cc15817c70929a5b9f3cfafe0
Diogo Rodrigues <up201806429@fe.up.pt>
2020-11-22 15:32:38 +0000

    Changed text and eval

--- TP2/MySpriteText.js
+++ TP2/MySpriteText.js
@@ -10,7 +10,13 @@ class MySpriteText{
         this.scene = scene;
         this.text = text;
         this.exp = exp;
-        this.geometry = new MyRectangle(this.scene, -0.5, -0.5, 0.5, 0.5, 1, 1);
+        if(this.exp != null) this.func = new Function(this.exp);
+        this.geometries = [];
+        for(let i = 0; i < text.length; ++i){
+            let x1 = -text.length/2 + i;
+            let x2 = x1+1;
+            this.geometries.push(new MyRectangle(this.scene, x1, -0.5, x2, +0.5, 1, 1));
+        }
         this.spriteSheet = new MySpriteSheet(scene, "scenes/spritesheets/oolite-font.png", 16, 16);
     }
     getCharacterPosition(character){
@@ -19,12 +25,11 @@ class MySpriteText{
     display(){
         this.scene.setActiveShaderSimple(this.spriteSheet.shader);
         if(this.exp != null)
-            eval(this.exp);
+            this.text = this.func();
         this.scene.pushMatrix();
         for (var i = 0; i < this.text.length; i++) {
             this.spriteSheet.activateCellP(this.getCharacterPosition(this.text[i]));
-            this.geometry.display();
-            this.scene.translate(1, 0, 0);
+            this.geometries[i].display();
         }
         this.scene.popMatrix();
         this.scene.setActiveShaderSimple(this.scene.defaultShader);
