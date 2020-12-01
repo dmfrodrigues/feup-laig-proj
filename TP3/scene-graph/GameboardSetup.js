2aea435d0b83dbd81420f5aa014d14b32b5d8fc7
Diogo Rodrigues <up201806429@fe.up.pt>
2020-12-01 17:21:07 +0000

    Created GameboardSetup

--- TP3/scene-graph/GameboardSetup.js
+++ TP3/scene-graph/GameboardSetup.js
@@ -3,6 +3,12 @@ class GameboardSetup {
         this._idObj = null;
         this._obj = null;
         this._transformation = null;
+        this._cells = new Array(9);
+        for(let i = 0; i < 9; ++i){
+            this._cells[i] = new Array(9);
+            for(let j = 0; j < 9; ++j)
+                this._cells[i][j] = null;
+        }
     }
     set idObj(id){ this._idObj = id; }
     get idObj(){ return this._idObj; }
@@ -10,4 +16,6 @@ class GameboardSetup {
     get obj(){ return this._obj; }
     set transformation(transformation){ this._transformation = transformation; }
     get transformation(){ return this._transformation; }
+    setCellPosition(i, j, v){ this._cells[i][j] = v; }
+    getCellPosition(i, j){ return this._cells[i][j]; }
 }
