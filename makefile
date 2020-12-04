5781acefcb9673d6c9aa8d50d7651ae755bad350
Diogo Rodrigues <up201806429@fe.up.pt>
2020-12-04 21:32:23 +0000

    Can now run server in swipl and sicstus

--- makefile
+++ makefile
@@ -1,12 +1,14 @@
+SHELL := /bin/bash
+
 GROUP	=T2_G04
 
-PROLOG=sicstus
+PROLOG=swipl
 
 all:
 	make -C server/feup-plog-tp1 PROLOG=$(PROLOG)
 
 test:
-	cd server && sicstus -l server.pl --goal "server." -- parallel &
+	(cd server && $(PROLOG) -l server.pl -- parallel)&
 	sleep 2
 	curl -d '{"command":"hello","args":{}}' -H "Content-Type: application/json" -X POST "localhost:8081"
 	@echo
@@ -14,7 +16,7 @@ test:
 	@echo
 	curl -d '{"command": "choose_move", "args": { "gamestate": { "board": [[  0,  6,  0,  0,  0,"nan","nan","nan","nan"],[  0,  0,  0,  0,  0, -6,"nan","nan","nan"],[  0,  0,  0,  0,  0,  0,  0,"nan","nan"],[ -6,  0,  0,  0,  0,  0,  0,  0,"nan"],[  0,  0,  0,  0,  0,  0,  0,  0,  0],["nan",  0,  0,  0,  0,  0,  0,  0,  6],["nan","nan",  0,  0,  0,  0,  0,  0,  0],["nan","nan","nan",  6,  0,  0,  0,  0,  0],["nan","nan","nan","nan",  0,  0,  0, -6,  0]], "turn": 1 }, "turn": 1, "level": 3, "n": 7}}' -H "Content-Type: application/json" -X POST "localhost:8081"
 	@echo
-	kill $$(ps a | grep "sicstus -l server.pl" | grep -v "grep" | awk '{print $$1;}')
+	kill -s SIGKILL $$(ps a | grep "$(PROLOG) -l server.pl" | grep -v "grep" | awk '{print $$1;}')
 
 zip: LAIG_TP1_$(GROUP).zip LAIG_TP2_$(GROUP).zip
 
---
+++ server/server_common.pl
@@ -0,0 +1,35 @@
+% Handles parsed HTTP requests
+% Returns 200 OK on successful aplication of parse_input on request
+% Returns 400 Bad Request on syntax error (received from parser) or on failure of parse_input
+handle_request(json([command=Command,args=Args]), json([response=Reply]), '200 OK') :-
+	handle_command(Command, Args, Reply),
+	!.
+handle_request(_, '', '400 Bad Request').
+
+% COMMANDS
+:-reconsult('feup-plog-tp1/src/move.pl').
+:-reconsult('feup-plog-tp1/src/choose_move.pl').
+
+:-
+	retract(base_directory(_)),
+	current_working_directory(CWD),
+	atom_concat(CWD, 'feup-plog-tp1/', BASE),
+	assert(base_directory(BASE)).
+
+handle_command(hello, json([]), hello).
+% handle_command(quit, json([]), ok).
+handle_command(
+	move,
+	json([board=Board, playermove=json([player=Player,pos=[PosI,PosJ],substacks=Substacks,dir=Dir,newpos=[NewPosI,NewPosJ]])]),
+	NewBoard
+) :-
+	Move = playermove(Player, PosI-PosJ, Substacks, Dir, NewPosI-NewPosJ),
+	move(Board, Move, NewBoard).
+handle_command(
+	choose_move,
+	json([gamestate=json([board=Board,turn=Turn]),turn=Turn,level=Level,n=N]),
+	json([player=Player,pos=[PosI,PosJ],substacks=Substacks,dir=Dir,newpos=[NewPosI,NewPosJ]])
+) :-
+	GameState = gamestate(Board, Turn),
+	Move = playermove(Player,PosI-PosJ,Substacks,Dir,NewPosI-NewPosJ),
+	choose_move(GameState, Turn, Level, N, Move). 
