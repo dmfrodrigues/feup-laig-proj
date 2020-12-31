20962403849070b663a6d536b8ec607818374fba
BrenoAccioly <up201800170@fe.up.pt>
2020-12-31 22:28:02 +0000

    Rotation animation

--- TP3/logic/MoveStackArc.js
+++ TP3/logic/MoveStackArc.js
@@ -1,5 +1,6 @@
 STACK_ANIM_TIME = 2.0
 ROTATE_ANIM_TIME = 1.5
+DELAY_TIME = 0.2;
 
 class MoveStackArc extends MoveStack{
     constructor(scene, gameboard){
@@ -13,8 +14,8 @@ class MoveStackArc extends MoveStack{
         this.pieceStacks = [];
         this.destCells = [];
         this.substacks = [];
-        this.notEmptyDestCells = [];
-        this.stacksRotated = [];
+        this.notEmptyDestStacks = [];
+        this.stacksRotated = {};
     }
 
 
@@ -26,29 +27,22 @@ class MoveStackArc extends MoveStack{
         return total;
     }
 
-    rotateStack(){
-
-    }
-
     moveSubstacks(origCell, substacks, destCells, notEmptyDestCells, notEmptyDestHeights){
         this.substacks = substacks;
         this.origCell = origCell;
         this.startTime = this.scene.time;
         for(let i = 0; i < substacks.length; i++){
-            this.pieceStacks.push(new PieceStack(this.scene, substacks[i] * Math.sign(origCell.stack.height)));
+            this.pieceStacks.push(new PieceStack(this.scene, Math.abs(substacks[i]) * Math.sign(origCell.stack.height)));
             this.pieceStacks[i].cell = this.origCell;
             destCells[i].visible = false;
             this.destCells.push(destCells[i]);
         }
 
         for(let i=0; i < notEmptyDestCells.length; i++){
-            if(Math.sign(origCell.stack.height) != Math.sign(notEmptyDestHeights)){
-                this.stacksRotated.push(new PieceStack(this.scene, notEmptyDestHeights[i]));
-                this.stacksRotated[i].cell = notEmptyDestCells[i];
-            }
-            else{
-                this.notEmptyDestCells.push(new PieceStack(this.scene, notEmptyDestHeights[i]));
-                this.notEmptyDestCells[i].cell =  notEmptyDestCells[i];
+            this.notEmptyDestStacks.push(new PieceStack(this.scene, notEmptyDestHeights[i]));
+            this.notEmptyDestStacks[i].cell =  notEmptyDestCells[i];
+            if(Math.sign(origCell.stack.height) != Math.sign(notEmptyDestHeights[i])){
+                this.stacksRotated[this.notEmptyDestStacks[i]] = new PieceStack(this.scene, notEmptyDestHeights[i]);
             }
         }
 
@@ -56,26 +50,22 @@ class MoveStackArc extends MoveStack{
 
     update(t){
         this.deltaTime = t - this.startTime;
-        if(this.deltaTime >= STACK_ANIM_TIME){
-            for(let i=0; i < this.destCells.length; i++){
-                this.destCells[i].visible = true;
-            }
+        if(this.deltaTime >= STACK_ANIM_TIME + DELAY_TIME){
             this.pieceStacks = [];
             this.destCells = [];
             this.substacks = [];
-            this.notEmptyDestCells = [];
-            this.stacksRotated = [];
-            return;
-        };
-    }
-
-    diplayRotation(){
-        
+            this.notEmptyDestStacks = [];
+            this.stacksRotated = {};
+        }      
+        else if(this.deltaTime >= STACK_ANIM_TIME){
+            for(let i=0; i < this.destCells.length; i++){
+                this.destCells[i].visible = true;
+            }
+        }
     }
 
     display(){
         for(let i=0; i < this.pieceStacks.length; i++){
-            
             let w = this.deltaTime / STACK_ANIM_TIME;
             let origPos = this.gameboard.gameboardSetup.getCellPosition(this.origCell.i, this.origCell.j);
             let destPos = this.gameboard.gameboardSetup.getCellPosition(this.destCells[i].i, this.destCells[i].j);
@@ -94,25 +84,30 @@ class MoveStackArc extends MoveStack{
 
         }
 
-        for(let i=0; i < this.stacksRotated.length; i++){
-            let w = this.deltaTime / ROTATE_ANIM_TIME;
+        for(let i=0; i < this.notEmptyDestStacks.length; i++){
+            if(this.stacksRotated[this.notEmptyDestStacks[i]] != undefined){
+                let w = this.deltaTime / ROTATE_ANIM_TIME;
 
-            let y = (1.0-Math.pow((this.deltaTime-1),2)) * 0.05;
-            let alpha = Math.min(1, w) * Math.PI;
-            
-            this.scene.pushMatrix();
-            this.scene.translate(0, y, 0);
-            //this.scene.multMatrix(this.gameboard.gameboardSetup.transformation);
-            this.scene.rotate(alpha, 1, 0, 0);
-            this.stacksRotated[i].display();
-
-            this.scene.popMatrix();
-        }
-
-        for(let i=0; i < this.notEmptyDestCells.length; i++){
-            this.scene.pushMatrix();
-            this.notEmptyDestCells[i].display();
-            this.scene.popMatrix();
+                let y = (1.0-Math.pow((this.deltaTime-1),2)) * 0.05;
+                let alpha = Math.min(1, w) * Math.PI;
+                
+                this.scene.pushMatrix();
+                this.scene.translate(0, y, 0);
+                this.scene.multMatrix(this.gameboard.gameboardSetup.getCellMatrix(
+                    this.notEmptyDestStacks[i].cell.i,
+                    this.notEmptyDestStacks[i].cell.j
+                    )
+                );
+                this.scene.rotate(alpha, 1, 0, 0);
+                this.stacksRotated[this.notEmptyDestStacks[i]].display();
+    
+                this.scene.popMatrix();
+            }
+            else{
+                this.scene.pushMatrix();
+                this.notEmptyDestStacks[i].display();
+                this.scene.popMatrix();
+            }
         }
     }
 }
